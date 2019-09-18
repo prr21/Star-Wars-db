3ae3d4d96e93977cb24736bbad55411b7e928902
prr <Mr.gustav009@gmail.com>
2019-09-18 23:45:44 +0300

    Иконка загрузки централизованна

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './loading.css';
 
 const ItemList = () => {
     return (
-        <div className="lds-css ng-scope">
+        <div className="lds-css ng-scope loading">
             <div style={{width: '100%',height:'100%'}} className="lds-rolling">
                 <div></div>
             </div>
