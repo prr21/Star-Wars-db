16cbf868cde509b33d3a24e986c65ba5eb08eb32
prr <Mr.gustav009@gmail.com>
2019-10-29 23:41:27 +0300

    Отдельный компонент обёртывания ошибки

---
+++ src/components/error-boundary/error-boundary.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import ErrorIndicator from '../error-indicator';
+
+import './error-boundary.css'
+
+export default class ErrorBoundary extends Component {
+
+	state = {
+		hasError: false
+	}
+
+	componentDidCatch(error,info){
+		console.log(info)
+
+		this.setState({
+			hasError: true
+		})
+	}
+
+	render(){
+		if (this.state.hasError) {
+			return <ErrorIndicator />
+		}
+		return this.props.children
+	}
+}
