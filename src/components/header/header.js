519e1b0340826683e21d4f96aef63924d318c609
prr <Mr.gustav009@gmail.com>
2019-11-09 23:50:25 +0300

    Добавлена кнопка смены API

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,18 +2,28 @@ import React from 'react';
 
 import './header.css';
 
-const Header = () => {
+const Header = ({ onToggleData }) => {
     return (
-        <div className="header d-flex">
-            <h3>
-                <a href="#prr">Star DB</a>
-            </h3>
+        <div className="header d-flex justify-content-between">
+            <div className="d-flex">
+                <h3>
+                    <a href="#prr">Star DB</a>
+                </h3>
 
-            <ul className="d-flex">
-                <li> <a href="#prr1">People</a> </li>
-                <li> <a href="#prr2">Planets</a> </li>
-                <li> <a href="#prr3">Starships</a> </li>
-            </ul>
+                <ul className="d-flex">
+                    <li> <a href="#prr1">People</a> </li>
+                    <li> <a href="#prr2">Planets</a> </li>
+                    <li> <a href="#prr3">Starships</a> </li>
+                </ul>
+            </div>
+
+            <div className="d-flex align-items-center">
+                <button 
+                    className="btn btn-md btn-primary"
+                    onClick={onToggleData}>
+                    Change Data
+                </button>
+            </div>
         </div>
     );
 };
