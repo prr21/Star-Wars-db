6a5dbc52d8368316c377a7f53dca9cdb2776a2ea
prr <Mr.gustav009@gmail.com>
2019-11-12 21:17:45 +0300

    Использование Switch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -10,7 +10,9 @@ import {StarshipDetails}from "../sw-components";
 import {
     PersonPage,
     PlanetPage,
-    StarshipPage
+    StarshipPage,
+    LoginPage,
+    SecretPage
 } from '../pages'
 
 import { SwapiProvider } from '../swapi-service-context'
@@ -22,7 +24,8 @@ export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
-        swapiResorse: new SwapiResorse()
+        swapiResorse: new SwapiResorse(),
+        isLogged: false
     }
 
     toggleRandomPlanet = () => {
@@ -30,6 +33,12 @@ export default class App extends Component {
             return {
                 showRandomPlanet: !state.showRandomPlanet
             }
+        })
+    }
+
+    onLogin = () => {
+        this.setState({
+            isLogged: true
         })
     }
 
@@ -47,7 +56,7 @@ export default class App extends Component {
 
     render(){
 
-        const { showRandomPlanet, swapiResorse } = this.state
+        const { showRandomPlanet, swapiResorse, isLogged } = this.state
 
         const planet = showRandomPlanet ?
             <RandomPlanet /> :
@@ -56,20 +65,21 @@ export default class App extends Component {
         return (
             <ErrorBoundary>
               <SwapiProvider value={swapiResorse}>
-                <Router >
+                <Router>
+                  <Header onToggleData={this.onToggleData}/>
 
-                    <Header onToggleData={this.onToggleData}/>
+                  { planet }
 
-                    { planet }
+                  <div className="row mb-2 button-row">
+                      <button
+                          className="toggle-planet btn btn-warning btn-lg"
+                          onClick={this.toggleRandomPlanet}>
+                          Toggle Random Planet
+                      </button>
+                      <ErrorBtn />
+                  </div>
 
-                    <div className="row mb-2 button-row">
-                        <button
-                            className="toggle-planet btn btn-warning btn-lg"
-                            onClick={this.toggleRandomPlanet}>
-                            Toggle Random Planet
-                        </button>
-                        <ErrorBtn />
-                    </div>
+                  <Switch>
                     <Route path='/' exact render={ () => 
                         <h2 className='text-center mt-4'>Welcome To Star DB Application!</h2>
                     } />
@@ -78,9 +88,25 @@ export default class App extends Component {
                     <Route path='/planets' component={ PlanetPage } />
                     <Route path='/starships' exact component={ StarshipPage } />
 
+                    <Route path='/login' exact render={ () =>
+                        <LoginPage isLogged={isLogged} login={this.onLogin}/>
+                    } />
+
+                    <Route path='/secret' exact render={ () =>
+                        <SecretPage isLogged={isLogged}/>
+                    } />
+
                     <Route path='/starships/:id' render={({ match }) => 
                         <StarshipDetails itemId={match.params.id} />
                     } />
+
+                    <Route render={ () => {
+                        return (
+                            <div className="text-center mt-4">
+                                <h3>404 – page not found</h3>
+                            </div>)}
+                    }/>
+                  </Switch>
 
                 </Router>
               </SwapiProvider>
