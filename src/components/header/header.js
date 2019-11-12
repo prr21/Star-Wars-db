df4272fcab3ffa3c3a468f1d5d23cda99bd3559c
prr <Mr.gustav009@gmail.com>
2019-11-12 21:17:10 +0300

    Новые экспорты и импорты

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,9 +12,11 @@ const Header = ({ onToggleData }) => {
                 </h3>
 
                 <ul className="d-flex">
-                    {<li> <Link to="/people/">People</Link> </li>}
-                    {<li> <Link to="/planets/">Planets</Link> </li>}
-                    {<li> <Link to="/starships/">Starships</Link> </li>}
+                    <li> <Link to="/people/">People</Link> </li>
+                    <li> <Link to="/planets/">Planets</Link> </li>
+                    <li> <Link to="/starships/">Starships</Link> </li>
+                    <li> <Link to="/login">Login</Link> </li>
+                    <li> <Link to="/secret">Secret</Link> </li>
                 </ul>
             </div>
 
