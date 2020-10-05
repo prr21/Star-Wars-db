31d4e5bc4cca42efd84429e676f73b85895f4600
Kurban <Mr.gustav009@gmail.com>
2020-10-05 20:58:37 +0300

    Production

---
+++ build/precache-manifest.e9294ca2415945a77a1947fd65653727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cb1aa112d0f4a292f51aa7e215c156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd3ce76769c5d20f8c0",
+    "url": "/static/css/main.8bec6463.chunk.css"
+  },
+  {
+    "revision": "f51631c19c5c47f5139c",
+    "url": "/static/js/2.62767a48.chunk.js"
+  },
+  {
+    "revision": "bdd3ce76769c5d20f8c0",
+    "url": "/static/js/main.cad338e8.chunk.js"
+  },
+  {
+    "revision": "3576e5166a29027fcab7",
+    "url": "/static/js/runtime~main.9eff4ad2.js"
+  },
+  {
+    "revision": "098eebd8bcf9034768c3202a843f40bb",
+    "url": "/static/media/death-star.098eebd8.png"
+  }
+]);
