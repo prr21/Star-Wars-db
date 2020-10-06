58fa78c5d9227a663d08e4ea885ac21804f7dc0f
Kurban <Mr.gustav009@gmail.com>
2020-10-06 09:37:27 +0300

    gh pages

---
+++ build/precache-manifest.a837e03938cb08768c3b4450a92856a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38371994c09116812e334dd80e7853b3",
+    "url": "/Star-Wars-db/build/index.html"
+  },
+  {
+    "revision": "bdd3ce76769c5d20f8c0",
+    "url": "/Star-Wars-db/build/static/css/main.8bec6463.chunk.css"
+  },
+  {
+    "revision": "f51631c19c5c47f5139c",
+    "url": "/Star-Wars-db/build/static/js/2.62767a48.chunk.js"
+  },
+  {
+    "revision": "bdd3ce76769c5d20f8c0",
+    "url": "/Star-Wars-db/build/static/js/main.cad338e8.chunk.js"
+  },
+  {
+    "revision": "039ddad84eab388b3057",
+    "url": "/Star-Wars-db/build/static/js/runtime~main.f1d94a3e.js"
+  },
+  {
+    "revision": "098eebd8bcf9034768c3202a843f40bb",
+    "url": "/Star-Wars-db/build/static/media/death-star.098eebd8.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9294ca2415945a77a1947fd65653727.js"
+  "/Star-Wars-db/build/precache-manifest.a837e03938cb08768c3b4450a92856a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Star-Wars-db/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
